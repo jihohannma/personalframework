17c792c04020c7cb2406a80410469ae1181e8513
jihohannma <hanjiho@hawaii.edu>
2021-10-31 21:06:53 -1000

    added custom bespoke scroll-triggered animation

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ mymenubutton.onclick = function() {
         mysitenav.setAttribute('data-navstate', 'open')
     }
 };
+
+
+// REVEAL ON SCROLL JS
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute('data-sectionstate', 'active');
+        } else {
+            entry.target.setAttribute('data-sectionstate', 'inactive');
+        }
+    });
+});
+document.querySelectorAll('.animate-on-scroll').forEach( (section) => {
+    myobserver.observe(section);
+});
